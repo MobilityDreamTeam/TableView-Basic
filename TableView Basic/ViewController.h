002712730f06fbd8e9cb2a9b4be6c61251bb841e
Per Friis <per.friis@friisdata.dk>
2015-03-18 22:19:17 +0100

    Initial Commit

---
+++ TableView Basic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableView Basic
+//
+//  Created by Per Friis on 18/03/15.
+//  Copyright (c) 2015 Mobility Dream Team aps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableView Basic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableView Basic
+//
+//  Created by Per Friis on 18/03/15.
+//  Copyright (c) 2015 Mobility Dream Team aps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
